9e54da82b246fe6b39771a12ab815e9a57f84555
Coral <coralruschak@gmail.com>
2024-10-30 18:15:43 -0400

    Refactor: player_setup runs with user input and returns error message & reprompts with invalid coordinates

--- lib/play.rb
+++ lib/play.rb
@@ -6,21 +6,43 @@ class Play
     @computer_board= Board.new
   end
 
-  def setup
+  def player_setup
     puts "I have laid out my ships on the grid. \n" + "You now need to lay out your two ships. \n" + "The Cruiser is three units long and the Submarine is two units long."
     puts @player_board.render(true)
+
+
     puts "Enter the squares for the Cruiser (3 spaces):"
-    gets.chomp
+    #cruiser_coordinates = gets.chomp
+    cruiser_coordinates = "A1 A2 A4"
+    while @player_board.valid_placement?(Ship.new("cruiser", 3), cruiser_coordinates.split(" ")) == false
+      puts "Those are invalid coordinates. Please try again:"
+      #cruiser_coordinates = gets.chomp
+      cruiser_coordinates = "A1 A2 A3"
+      @player_board.valid_placement?(Ship.new("cruiser", 3), cruiser_coordinates.split(" "))
+    end
+    @player_board.place(Ship.new("cruiser", 3), cruiser_coordinates.split(" "))
     puts @player_board.render(true)
-    puts "Enter the squares for the Submarine (2 spaces): \n"
 
+
+    puts "Enter the squares for the Submarine (2 spaces):"
+    #submarine_coordinates = gets.chomp
+    submarine_coordinates = "B1 B3"
+    while @player_board.valid_placement?(Ship.new("submarine", 2), submarine_coordinates.split(" ")) == false
+      puts "Those are invalid coordinates. Please try again:"
+      #submarine_coordinates = gets.chomp
+      submarine_coordinates = "B1 B2"
+      @player_board.valid_placement?(Ship.new("submarine", 2), submarine_coordinates.split(" "))
+    end
+    player_board.place(Ship.new("submarine", 2), submarine_coordinates.split(" "))
+    puts @player_board.render(true)
   end
+  
   def turn
-    
+
   end
 
-  def end_game
-    
+  def game_over
+
   end
 
 end
