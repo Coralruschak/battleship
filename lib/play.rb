e9875ccd24f37707d686bdd68ef21767cbb99309
Coral <coralruschak@gmail.com>
2024-10-30 01:48:46 -0400

    feat: game setup messages for ship placement

--- lib/play.rb
+++ lib/play.rb
@@ -5,4 +5,15 @@ class Play
     @player_board = Board.new
     @computer_board= Board.new
   end
+
+  def setup
+    puts "I have laid out my ships on the grid. \n" + "You now need to lay out your two ships. \n" + "The Cruiser is three units long and the Submarine is two units long."
+    puts @player_board.render(true)
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    gets.chomp
+    puts @player_board.render(true)
+    puts "Enter the squares for the Submarine (2 spaces): \n"
+
+  end
+
 end
